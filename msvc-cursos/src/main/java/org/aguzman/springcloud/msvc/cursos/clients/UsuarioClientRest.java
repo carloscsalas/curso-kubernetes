c02c46c71bee99770f103aa0b548d86dd862bfbd
Gino Fernandez <jaime.fernandez.miranda@bbva.com>
2023-07-30 22:04:40 -0500

    Docker:Introduccion

---
+++ msvc-cursos/src/main/java/org/aguzman/springcloud/msvc/cursos/clients/UsuarioClientRest.java
@@ -0,0 +1,30 @@
+package org.aguzman.springcloud.msvc.cursos.clients;
+
+import org.aguzman.springcloud.msvc.cursos.models.Usuario;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+
+/*FeignClient, nos permite consumir al microservicio usuario.
+name, nombre del microservicio. url, url base del microservicio*/
+@FeignClient(name = "msvc-usuarios", url = "localhost:8000")
+public interface UsuarioClientRest {
+
+    @GetMapping("/{id}")
+    Usuario detalle(@PathVariable Long id);
+
+    @PostMapping("/")
+    Usuario crear(@RequestBody Usuario usuario);
+
+    /**Se estuvo utilizando List en el RequestParam, pero en el Feign da problemas, por ello
+     * utilizamos Iterable. Al final Iterable es una interface que se implementa en el List*/
+    @GetMapping("/usuarios-por-curso")
+    List<Usuario> obtenerAlumnosPorCursos(@RequestParam Iterable<Long> ids);
+
+}
---
+++ msvc-usuarios/src/main/java/org/aguzman/springcloud/msvc/usuarios/services/UsuarioServiceImpl.java
@@ -0,0 +1,62 @@
+package org.aguzman.springcloud.msvc.usuarios.services;
+
+import org.aguzman.springcloud.msvc.usuarios.clients.CursoClientRest;
+import org.aguzman.springcloud.msvc.usuarios.models.entity.Usuario;
+import org.aguzman.springcloud.msvc.usuarios.repositories.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UsuarioServiceImpl implements UsuarioService{
+
+    @Autowired
+    private UsuarioRepository repository;
+
+    @Autowired
+    private CursoClientRest clientRest;
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<Usuario> listar() {
+        return (List<Usuario>) repository.findAll();
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public Optional<Usuario> porId(Long id) {
+        return repository.findById(id);
+    }
+
+    @Override
+    @Transactional
+    public Usuario guardar(Usuario usuario) {
+        return repository.save(usuario);
+    }
+
+    @Override
+    @Transactional
+    public void eliminar(Long id) {
+        repository.deleteById(id);
+        clientRest.eliminarCursoUsuarioPorId(id);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<Usuario> listarPorIds(Iterable<Long> ids) {
+        return (List<Usuario>) repository.findAllById(ids);
+    }
+
+    @Override
+    public Optional<Usuario> getByEmail(String email) {
+        return repository.obtenerPorEmail(email);
+    }
+
+    @Override
+    public boolean existePorEmail(String email) {
+        return repository.existsByEmail(email);
+    }
+}
